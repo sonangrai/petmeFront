6b00f612203945535d1f9d12144e9c04ee38a67b
sonangrai <shonahangrae@gmail.com>
2022-01-30 18:00:10 +0545

    profile sagas done

---
+++ redux/sagas/profileSagas.js
@@ -0,0 +1,24 @@
+import { all, takeLatest } from "@redux-saga/core/effects";
+import { getProfileApi } from "../api/profile";
+import {
+  gettingProfile,
+  gettingProfileError,
+  gettingProfileSuccess,
+} from "../reducers/actions/authActions";
+
+/**
+ * Get profile
+ */
+function* getProfileSaga(action) {
+  try {
+    let res = yield call(getProfileApi, action.payload);
+    console.log(res);
+    yield put(gettingProfileSuccess(res));
+  } catch (error) {
+    yield put(gettingProfileError(error.response));
+  }
+}
+
+export default function* profileSagas() {
+  yield all([takeLatest(gettingProfile, getProfileSaga)]);
+}
