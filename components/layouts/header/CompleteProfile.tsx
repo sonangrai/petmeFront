6690704ccd8e90717ac4e19f150ebd7b300186d9
sonangrai <shonahangrae@gmail.com>
2022-03-05 09:19:07 +0545

    got ride of useRouter

--- components/layouts/header/CompleteProfile.tsx
+++ components/layouts/header/CompleteProfile.tsx
@@ -1,18 +1,14 @@
 import { NoProfile } from "./styled/HeadStyle.styled";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 /**
  *
  * @returns No profile component
  */
 const CompleteProfile = ({ auth }) => {
-  let router = useRouter();
-
   //Checking if the page is edit page or not and also checking profile is not added from state
   if (
-    (router.asPath !== "/profile/edit" &&
-      auth.gettingProfileError.status === 404) ||
+    auth.gettingProfileError.status === 404 ||
     auth.profile.firstname == undefined
   )
     return (
